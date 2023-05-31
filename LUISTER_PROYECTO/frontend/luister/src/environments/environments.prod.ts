9e55fcb477d5eb444da93dc556686c5e1fcd0928
faustoobama <faustoobama@outlook.com>
2023-05-31 13:04:10 +0200

    creacion del environments para produccion

--- LUISTER_PROYECTO/frontend/luister/src/environments/environments.prod.ts
+++ LUISTER_PROYECTO/frontend/luister/src/environments/environments.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: true,
     sfyUrl: 'https://api.spotify.com/v1/',
     sfyPxUrl: '/api1/',
+    dzrUrl: '/api2/',
     ID: '228337ef0a124d5ebe912338d8050e03',
     SID: 'be4da263ec534f46bdf841e70bcf5f94',
     sfyKU: '/api1/api/token/',
