2d0976fbe482d4b1f7dad6a21e70c2569e3833b0
HDavo <dabghos@outlook.com>
2023-06-02 22:57:01 +0200

    Avances en material

--- version_material/backend/alternativo/luister.sql
+++ version_material/backend/alternativo/luister.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2023 a las 23:06:36
+-- Tiempo de generación: 25-05-2023 a las 01:09:37
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.0.25
 
@@ -17,11 +17,21 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-DROP DATABASE IF EXISTS `luister`;
+--
+-- Base de datos: `luister`
+--
 
-CREATE DATABASE IF NOT EXISTS `luister` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- --------------------------------------------------------
 
-USE `luister`;
+DROP DATABASE IF EXISTS luister;
+
+CREATE  DATABASE luister;
+
+USE luister;
+
+--
+-- Estructura de tabla para la tabla `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -31,30 +41,35 @@ CREATE TABLE `users` (
   `creationdate` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `creationdate`) VALUES
-(1, 'Lorenzo', 'dorianwalkler24@gmail.com', '$2y$10$lPSytB9C3aUVLO5n3jDtrOs66gx78R8eeuo0XIuggEfa9vnug7HGy', '2023-05-27 12:19:17');
-
+--
+-- Estructura de usersettings
+--
 CREATE TABLE `usersettings` (
 `key` VARCHAR(25) NOT NULL,
 `value` BOOLEAN NOT NULL,
 `userid` INT NOT NULL,
-CONSTRAINT fk_usersettings_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CONSTRAINT FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
+);
+
+--
+-- Estructura de tabla para la tabla `customlists`
+--
 
 CREATE TABLE `customlists` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `title` varchar(55) NOT NULL,
-  `description` text NOT NULL,
+  `description` text DEFAULT NULL,
   `image` varchar(100) DEFAULT NULL,
   `userid` int(11) NOT NULL,
   `creationdate` datetime NOT NULL DEFAULT current_timestamp(),
   CONSTRAINT fk_customlists_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE  CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `customlists` (`id`, `title`, `description`, `image`, `userid`, `creationdate`) VALUES
-(2, 'rock alternativo', 'canciones de rock alternativo', 'alt-rok.jpg', 1, '2023-05-27 17:57:19'),
-(3, 'japon', '', 'con-la-primavera-in-giappone.jpg', 1, '2023-05-28 10:30:48'),
-(5, 'africanas', '', 'eg.jpg', 1, '2023-05-28 10:34:15');
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `customlisttracks`
+--
 
 CREATE TABLE `customlisttracks` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -63,38 +78,46 @@ CREATE TABLE `customlisttracks` (
   `customlistid` int(11) NOT NULL,
   `lookupkey` varchar(100) NOT NULL,
   `includedon` datetime NOT NULL DEFAULT current_timestamp(),
-  `album` varchar(100) NOT NULL,
-   CONSTRAINT fk_customlisttracks_customlistid FOREIGN KEY (customlistid) REFERENCES customlists(id) ON DELETE  CASCADE
+  CONSTRAINT fk_cltracks_customlistid FOREIGN KEY (customlistid) REFERENCES customlists(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `customlisttracks` (`id`, `title`, `artist`, `customlistid`, `lookupkey`, `includedon`, `album`) VALUES
-(9, 'Falling Inside the Black', 'Skillet', 2, 'dzr:707229', '2023-06-01 11:39:42', ''),
-(10, 'Animal I Have Become', ', Three Days Grace', 2, 'sfy:56sk7jBpZV0CD31G9hEU3b', '2023-06-01 17:19:19', ''),
-(11, 'Its All Over', 'Three Days Grace', 2, 'sfy:2QZxpZDfJjhEGclSszvMVX', '2023-06-01 17:21:18', ''),
-(12, 'Ojo de Halcón', 'Hoke, Louis Amoeba', 3, 'sfy:0VhfZo2uwcWnQGExuOxNKq', '2023-06-01 17:23:09', ''),
-(13, 'Animal I Have Become', 'Three Days Grace', 3, 'sfy:56sk7jBpZV0CD31G9hEU3b', '2023-06-01 18:59:14', ''),
-(14, 'Monster', 'Skillet', 2, 'dzr:4116951', '2023-06-01 19:05:50', '');
+--
+-- Volcado de datos para la tabla `customlisttracks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritetracks`
+--
 
 CREATE TABLE `favoritetracks` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `title` varchar(55) NOT NULL,
-  `artist` varchar(100) NOT NULL,
-  `album` varchar(100) NOT NULL,
   `userid` int(11) NOT NULL,
   `lookupkey` text NOT NULL,
-  `includedon` datetime NOT NULL DEFAULT current_timestamp(),
-  CONSTRAINT fk_favoritetracks_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE  CASCADE
+  `name` varchar(55) NOT NULL,
+  `likedon` datetime NOT NULL DEFAULT current_timestamp(),
+  CONSTRAINT fk_favtracks_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE  CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `followedartists`
+--
 
 CREATE TABLE `followedartists` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(25) NOT NULL,
-  `image` text NOT NULL,
   `follower` int(11) NOT NULL,
-  `lookupkey` text NOT NULL,
   `followedon` datetime NOT NULL DEFAULT current_timestamp(),
-  CONSTRAINT fk_followedartists_follower FOREIGN KEY (follower) REFERENCES users(id) ON DELETE CASCADE
+  `lookupkey` text NOT NULL,
+  `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `passwordresettokens`
+--
 
 CREATE TABLE `passwordresettokens` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -102,13 +125,22 @@ CREATE TABLE `passwordresettokens` (
   `userid` int(11) NOT NULL,
   `creationdate` datetime NOT NULL DEFAULT current_timestamp(),
   `expirationdate` datetime NOT NULL,
-  CONSTRAINT fk_passwrdresettoken_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
+  CONSTRAINT fk_passrestokens_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE  CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Disparadores `passwordresettokens`
+--
 DELIMITER $$
 CREATE TRIGGER `resetpass_insert_expiration` BEFORE INSERT ON `passwordresettokens` FOR EACH ROW SET NEW.expirationdate = DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sessions`
+--
 
 CREATE TABLE `sessions` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -117,66 +149,57 @@ CREATE TABLE `sessions` (
   `device` longtext NOT NULL,
   `creationdate` datetime NOT NULL DEFAULT current_timestamp(),
   `expirationdate` datetime NOT NULL,
-  CONSTRAINT fk_sessions_userid FOREIGN KEY (userid) REFERENCES users(id)
+  CONSTRAINT fk_sessions_userid FOREIGN KEY (userid) REFERENCES users(id) ON DELETE  CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- Disparadores `sessions`
+--
 DELIMITER $$
 CREATE TRIGGER `session_exp_date` BEFORE INSERT ON `sessions` FOR EACH ROW SET NEW.expirationdate =  DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 7 DAY)
 $$
 DELIMITER ;
 
-CREATE TABLE `trackalbums` (
-  `lookupkey` varchar(100) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
+--
+-- Índices para tablas volcadas
+--
 
-
-CREATE TABLE `trackartists` (
-  `lookupkey` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-DELIMITER $$
-CREATE TRIGGER `remove_track_artist` AFTER DELETE ON `customlisttracks` FOR EACH ROW DELETE FROM `trackartists` WHERE OLD.artist = lookupkey
-$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE TRIGGER `remove_track_album` AFTER DELETE ON `customlisttracks` FOR EACH ROW DELETE FROM `trackalbums` WHERE OLD.album = lookupkey
-$$
-DELIMITER ;
-
+--
+-- Indices de la tabla `customlists`
+--
 ALTER TABLE `customlists`
-  ADD UNIQUE KEY `title` (`title`,`userid`);
+  ADD UNIQUE KEY `title` (`title`);
 
+--
+-- Indices de la tabla `customlisttracks`
+--
 ALTER TABLE `customlisttracks`
   ADD UNIQUE KEY `title` (`title`,`artist`,`customlistid`);
 
+--
+-- Indices de la tabla `favoritetracks`
+--
 ALTER TABLE `favoritetracks`
-  ADD UNIQUE KEY `userid` (`userid`,`title`,`artist`);
+  ADD UNIQUE KEY `userid` (`lookupkey`) USING HASH;
 
+--
+-- Indices de la tabla `followedartists`
+--
 ALTER TABLE `followedartists`
   ADD UNIQUE KEY `name` (`name`,`follower`);
 
-ALTER TABLE `passwordresettokens`
-  ADD UNIQUE KEY `value` (`value`) USING HASH;
-
-ALTER TABLE `sessions`
-  ADD UNIQUE KEY `token` (`token`) USING HASH;
-
-ALTER TABLE `trackalbums`
-  ADD UNIQUE KEY `lookupkey` (`lookupkey`,`title`);
-
-ALTER TABLE `trackartists`
-  ADD UNIQUE KEY `lookupkey` (`lookupkey`,`name`);
-
+--
+-- Indices de la tabla `users`
+--
 ALTER TABLE `users`
-  ADD UNIQUE KEY `email` (`email`);
+ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `usersettings`
+--
 
 ALTER TABLE `usersettings`
-  ADD UNIQUE KEY `userid_key_value`(`key`,`value`,`userid`);
-
-COMMIT;
+ADD UNIQUE KEY `userid_key_value`(`key`,`value`,`userid`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
