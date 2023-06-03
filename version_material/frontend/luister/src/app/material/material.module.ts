8f22afd4fb4fa4a0f60359292915b20bbfef5685
HDavo <dabghos@outlook.com>
2023-06-03 23:33:28 +0200

    Busqueda en lista completa, selects pendiente

--- version_material/frontend/luister/src/app/material/material.module.ts
+++ version_material/frontend/luister/src/app/material/material.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { MatTableModule } from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSortModule} from '@angular/material/sort';
@@ -10,6 +9,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
 
 
 
@@ -26,6 +26,7 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     MatInputModule,
     MatButtonModule,
     MatSlideToggleModule,
+    MatSelectModule
 
   ],
   exports: [
@@ -37,6 +38,7 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     MatInputModule,
     MatButtonModule,
     MatSlideToggleModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
