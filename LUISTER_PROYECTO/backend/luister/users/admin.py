7c98cc58e86432aa09023db3940baf786eb7a3f9
faustoobama <faustoobama@outlook.com>
2023-06-05 13:24:57 +0200

    Ajuste de campos de la tabla login de Django ==> AbstractBaseUser

--- LUISTER_PROYECTO/backend/luister/users/admin.py
+++ LUISTER_PROYECTO/backend/luister/users/admin.py
@@ -1,27 +1,24 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import BaseUserManager
 from users.models import Users
 
 admin.site.register(Users)
 
-"""
 class UserAdmin(admin.ModelAdmin):
 
-    list_display = ('pk', 'username', 'email',)
-    list_display_links = ('pk', 'username', 'email',)
+    list_display = ('id', 'name', 'email',)
+    list_display_links = ('id', 'name', 'email',)
 
-    search_fields = (
-        'email',
-        'name',
-    )
+    search_fields = ('email', 'name',)
 
-    list_filter = (
-        'is_active',
-        'is_staff',
-        'date_joined',
-        'modified',
-    )
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
 
-    readonly_fields = ('date_joined', 'modified',)
+    # list_filter = (
+    #     'is_active',
+    #     'is_staff',
+    #     'date_joined',
+    #     'modified',
+    # )
 
-"""
+    #readonly_fields = ('date_joined', 'modified',)
